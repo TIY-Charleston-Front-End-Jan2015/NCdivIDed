c348c22c9dc13bca9915c439d59e49d176dd4eb8
Rachel <BlantonAlmeida@gmail.com>
2015-01-30 14:45:52 -0500

    added js/jquery/scss to project

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # group1 divIDed
+var sports = {
+  name: 
+  Bio:
+},
+{
+  name:
+  Bio:
+}
+{
+  name:
+  Bio:
+}
+{
+  name:
+  Bio:
+}
+
+var music = {
+  name:
+  Bio:
+  },
+  {
+    name:
+    Bio:
+  }
+  {
+    name:
+    Bio:
+  }
+  {
+    name:
+    Bio:
+  }
+
+
+  var tech = {
+    name:
+    Bio:
+    },
+    {
+      name:
+      Bio:
+    }
+    {
+      name:
+      Bio:
+    }
+    {
+      name:
+      Bio:
+    }
---
+++ main.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+  //all code that is dom dependent lives here
+
+
+  //CREATING EVENTS///
+
+
+  ////BILL MURRAY/////////////
+
+
+  $(".box").find("select").on("change", function () {
+
+    if ($(this).val() === "AnswerA") {
+
+      $(this).closest(".box").css("background", "url(http://www.fillmurray.com/200/200)");
+
+    }
+
+    else if ($(this).val() === "AnswerB") {
+
+      $(this).closest(".box").css("background", "url(images/guessagain.jpg)");
+
+    }
+    else if ($(this).val() === "AnswerC") {
+
+      $(this).closest(".box").css("background", "url(images/guessagain.jpg)");
+
+    }
+    else { $(this).closest(".box").css("background", "url(images/defaultbg.jpg)");
+
+  }
+
+});
+
+///////NICK CAGE/////////
+
+$(".box2").find("select").on("change", function () {
+  if ($(this).val() === "AnswerA") {
+    $(this).closest(".box2").css("background", "url(http://www.placecage.com/200/200)");
+  }
+  else if ($(this).val() === "AnswerB") {
+    $(this).closest(".box2").css("background", "url(images/guessagain.jpg)");
+  }
+  else if ($(this).val() === "AnswerC") {
+    $(this).closest(".box2").css("background", "url(images/guessagain.jpg)");
+  }
+  else { $(this).closest(".box2").css("background", "url(images/defaultbg.jpg)");
+}
+});
+
+//CLOSES THE JAVASCRIPT///
+
+});
